3986797673b78f359e7c51ceb52a5783bb2ffa47
wonderfulnx <naxinlegend@outlook.com>
2019-06-08 21:28:48 +0800

    homework update: updated doc

--- fpproject-2019-05-17/app/Main.hs
+++ fpproject-2019-05-17/app/Main.hs
@@ -1,12 +1,4 @@
 module Main where
 
-import AST
-import EvalValue
-import EvalType
-
-pro1 = Program [] $ EEq (EIntLit 42) (EIntLit 42)
-
-pro2 = Program [] $ EEq (EIntLit 42) (ECharLit 'A')
-
 main :: IO ()
 main = undefined
